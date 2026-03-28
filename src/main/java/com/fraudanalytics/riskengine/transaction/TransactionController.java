1bfd77264b0115e336d718bbcd7c194c92d1a82a
Ashish <ar7011280548@gmail.com>
2026-03-28 12:42:16 +0530

    removed one file

--- src/main/java/com/fraudanalytics/riskengine/transaction/TransactionController.java
+++ src/main/java/com/fraudanalytics/riskengine/transaction/TransactionController.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
 @RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:5173")
 public class TransactionController {
 
     private final TransactionService transactionService;
